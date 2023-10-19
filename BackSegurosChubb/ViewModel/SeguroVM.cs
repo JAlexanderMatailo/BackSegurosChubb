f99d4f31e5fc64c8a1d20bb853dbbe2af105912c
JAlexanderMatailo <52136249+JAlexanderMatailo@users.noreply.github.com>
2023-10-19 12:19:27 -0500

    creacion clase SeguroVM en ViewModel

---
+++ BackSegurosChubb/ViewModel/SeguroVM.cs
@@ -0,0 +1,17 @@
+﻿namespace BackSegurosChubb.ViewModel
+{
+    public class SeguroVM
+    {
+        public int IdSeguros { get; set; }
+
+        public string NombreSeguro { get; set; }
+
+        public string Codigo { get; set; }
+
+        public double SumaAsegurada { get; set; }
+
+        public double Prima { get; set; }
+
+        public string Estado { get; set; }
+    }
+}

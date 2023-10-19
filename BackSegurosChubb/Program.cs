065ccb694f747ab8f427c8d3221438f9f9eb88e9
JAlexanderMatailo <52136249+JAlexanderMatailo@users.noreply.github.com>
2023-10-19 12:25:01 -0500

    metodo de conexion a base de datos en Program

--- BackSegurosChubb/Program.cs
+++ BackSegurosChubb/Program.cs
@@ -1,3 +1,6 @@
+using BackSegurosChubb.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,33 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Conexion Base de Datos
+
+builder.Services.AddDbContext<PruebaSegurosChubbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetSection("AppSettings").GetSection("DefaultConnection").Value);
+
+});
+
+var appSettingsSection = builder.Configuration.GetSection("AppSettings");
+
+#endregion
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "API",
+                      builder =>
+                      {
+                          builder.WithHeaders("*");
+                          builder.WithOrigins("*");
+                          builder.WithMethods("*");
+
+                      });
+});
+
+
+
 
 var app = builder.Build();
 
@@ -19,6 +49,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("API");
 
 app.MapControllers();
 
